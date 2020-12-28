82c2d0f48b167c26a9ff3b898511bc4a927bfd0c
YoloGamer123415 <jorambuitenhuis@gmail.com>
2020-12-28 11:48:15 +0100

    Moved header and footer to seperate files

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+	<span>Created by <a href="https://github.com/YoloGamer132415/" target="_blank">@YoloGamer123415</a> and <a href="https://github.com/idiidk/" target="_blank">@idiidk</a>.</span>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<header>
+	<nav>
+		<div class="left">
+			<a href="/index.html">
+				<img src="/resources/images/logo.png" alt="LOGO" />
+			</a>
+		</div>
+
+		<input type="checkbox" id="menu-toggle" hidden />
+		<label for="menu-toggle" class="menu-btn">
+			<span class="btn"></span>
+		</label>
+
+		<div class="right">
+			<ul>
+				<li>
+					<a href="/genre/index.html">Films</a>
+					<ul>
+						<li><a href="/genre/index.html#action">Actie</a></li>
+						<li><a href="/genre/index.html#comedy">Comedie</a></li>
+						<li><a href="/genre/index.html#horror">Horror</a></li>
+						<li><a href="/genre/index.html#romance">Romantiek</a></li>
+						<li><a href="/genre/index.html#oldskool">Old-Skool</a></li>
+					</ul>
+				</li>
+				<li><a href="/inloggen.html">Inloggen</a></li>
+			</ul>
+		</div>
+	</nav>
+</header>
