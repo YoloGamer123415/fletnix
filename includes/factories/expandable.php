8659854ecc97a1b42edd4df6243e7fc2a0165f53
YoloGamer123415 <jorambuitenhuis@gmail.com>
2020-12-28 13:11:30 +0100

    Added start to expandables

---
+++ includes/factories/expandable.php
@@ -0,0 +1,4 @@
+<?php
+function getExpandableHtml($genre, $movies) {
+	
+}
---
+++ movies/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="./resources/style/normalize.css" />
+	<link rel="stylesheet" href="./resources/style/variables.css" />
+	<link rel="stylesheet" href="./resources/style/main.css" />
+	<link rel="stylesheet" href="./resources/style/fonts.css" />
+	<title>Fletnix - Films</title>
+</head>
+<body>
+	<?php
+		include("../includes/header.php");
+	?>
+
+	<main>
+		<div class="container">
+			<h1>Hello, World!</h1>
+		</div>
+	</main>
+
+	<?php
+		include("../includes/header.php");
+	?>
+</body>
+</html>
