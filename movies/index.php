d0e6cc12057777c2d13adb509fef1cc39a83daaa
idiidk <dviide@protonmail.com>
2021-01-13 14:54:31 +0100

    require_once for queries.php

--- movies/index.php
+++ movies/index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require("../includes/helpers/queries.php");
+require_once("../includes/helpers/queries.php");
 require("../includes/factories/expandable.php");
 
 function getOptions($genres) {
